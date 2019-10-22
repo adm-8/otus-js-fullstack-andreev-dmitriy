cfa1bf5ecd521fd770c6529b50c4cb095954d894
Diman Andreev <dslads@gmail.com>
2019-10-22 16:22:15 +0300

    some changes

--- HW_5_Lesson_6/request.js
+++ HW_5_Lesson_6/request.js
@@ -8,29 +8,50 @@ var myXMLHttpRequest = (
 
 ) => {
 
+    let fetchResp = (resp) => {
+        console.log(resp);
+    }
+
     console.log(`Стартуем запуск ${reqCnt} запрос(ов) методом ${method} по адресу ${URL} в ${async ? 'асинхронном' : 'синхронном'} режиме...`);
 
-    // создаем экземпляр XMLHttpRequest
-    let xhr = new XMLHttpRequest();
+  
+
    
     // reqCnt раз
     for(let i=0; i < reqCnt; i++){
-        // настраиваем его
-        xhr.open(method, URL, async);
 
-        // и отправляем запрос
-        xhr.send();
-        console.log(`Request ${i+1} sent...`);
+        try{
+              // создаем экземпляр XMLHttpRequest
+            let xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState === 4) {
+                    fetchResp(`Запрос был обработан за ${xhr.response} мс`);
+                    }
+            }
+
+            // передаем параметры запроса
+            xhr.open(method, URL, async);
+                
+            // и отправляем запрос
+            xhr.send();
+            console.log(`Запрос ${i+1} был отправлен...`) ;
+
+        }catch(e){
+            console.log(`Ошибка при отправке запроса №${i+1} : ${e.toString()}`)
+        }
     }
-    
- 
+
 };
 
 
-// посылаем 100 запросов сначала асинхронно
-myXMLHttpRequest('POST', 'http://127.0.0.1:3000/get_some_page', 100);
+// засекаем время и посылаем запросы сначала асинхронно
+var req_start = Date.now();
+myXMLHttpRequest('POST', 'http://127.0.0.1:3000/get_some_page', 10);
+console.log(`Асинхронные запросы были отправлены за ${Date.now() - req_start} мс`);
 
 
-// затем посылаем 100 запросов синхронно
-myXMLHttpRequest('POST', 'http://127.0.0.1:3000/get_some_page', 100, false);
+// затем синхронно
+req_start = Date.now();
+myXMLHttpRequest('POST', 'http://127.0.0.1:3000/get_some_page', 10, false);
+console.log(`А вот синхронные запросы были отправлены уже за ${Date.now() - req_start} мс`);
 
